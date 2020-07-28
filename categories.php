3998773565150b9b2205b075b64f06693909682a
Ahmed-Gamal-Helal <ahmdtawfiek@gmail.com>
2020-07-28 22:17:52 +0200

    categories page

--- categories.php
+++ categories.php
@@ -3,10 +3,93 @@
 ?>
 <div class="container">
     <div class="row">
-        <div class="col-md-2">
-            asdas
+        <div class="col-md-3">
+            <section class="global-head">
+                <article>
+                    <h5><strong>Categories</strong></h5>
+                    <p><small>Select what you want</small></p>
+                </article>
+            </section>
+            <section class="collapse-area">
+                <div class="panel-group" id="accordion">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+                            Men</a>
+                        </h4>
+                        </div>
+                        <div id="collapse1" class="panel-collapse collapse in">
+                            <div class="panel-body">
+                                <div class="checkbox">
+                                    <label><input type="checkbox" value="">Pants</label>
+                                </div>
+                                <div class="checkbox">
+                                    <label><input type="checkbox" value="">Rolex</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+                            Women</a>
+                        </h4>
+                        </div>
+                        <div id="collapse2" class="panel-collapse collapse">
+                            <div class="panel-body">
+                                <div class="checkbox">
+                                    <label><input type="checkbox" value="">Perfume</label>
+                                </div>
+                                <div class="checkbox">
+                                    <label><input type="checkbox" value="">Clothes</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="mt-20">
+                <h4><strong>Country</strong></h4>
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search by name ..">
+                    <span class="input-group-btn">
+                        <button class="btn btn-primary" type="button"><i class="fa fa-search"></i></button>
+                    </span>
+                </div>
+            </section>
+            <section class="mt-20">
+                <h4><strong>Filter For</strong></h4>
+                <div class="radio">
+                    <label><input type="radio" name="filterfor">Newest</label>
+                    </div>
+                    <div class="radio">
+                    <label><input type="radio" name="filterfor">Best Offer</label>
+                    </div>
+                    <div class="radio">
+                    <label><input type="radio" name="filterfor">Best Selling</label>
+                </div>
+            </section>
         </div>
-        <div class="col-md-10">
+        <div class="col-md-9">
+            <div class="row">
+                <div class="col-md-10 px-0">
+                    <section class="mt-20">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search for products ..">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="button"><i class="fa fa-search"></i></button>
+                            </span>
+                        </div>
+                    </section>
+                </div>
+                <div class="col-md-2 text-center">
+                    <a href="new-ad.php">
+                        <button class="btn btn-primary mt-20" width="100%" type="button"><small>New Ad <i class="fa fa-plus px-5"></i></small></button>
+                    </a>
+                </div>
+            </div>
             <section class="global-head">
                 <article>
                     <h4><strong>Explore New Deals Today</strong></h4>
