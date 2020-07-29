67624672e0ab714281248fe63fe6a3ee441b3e2f
Aliaa99 <aliaaahmed129@gmail.com>
2020-07-29 20:44:25 +0200

    merge

---
+++ place-ad.php
@@ -0,0 +1,28 @@
+<?php
+    include 'controllers/header.php';
+?>
+    <div class="container">
+                <!-- place an Ad -->
+        <section class="global-head">
+            <article>
+                <h4><strong>Place an Ad</strong></h4>
+                <p>Please make sure you add all details to your Ad</p>
+            </article>
+        </section>
+        <!--  edit form to add details to Ad -->
+        <form action="">
+            <div class="row">
+                <div class="form-group">
+                    <div class="col-md-2">
+                        <label for="title">Title</label>
+                    </div>
+                    <div class="col-md-10">
+                        <input type="text" class="form-control" id="title" placeholder="search about products">
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+<?php
+    include 'controllers/footer.php';
+?>
