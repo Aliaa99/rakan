7158e94ef24f7d3f6bebfd85a3188d87015a63a2
Ahmed-Gamal-Helal <ahmdtawfiek@gmail.com>
2020-07-30 16:47:23 +0200

    merge

--- js/main.js
+++ js/main.js
@@ -33,7 +33,27 @@ $(function() {
         });
     });
 
+    var $star_rating = $('.star-rating .fa');
 
+    var SetRatingStar = function() {
+    return $star_rating.each(function() {
+        if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
+        return $(this).removeClass('fa-star-o').addClass('fa-star');
+        } else {
+        return $(this).removeClass('fa-star').addClass('fa-star-o');
+        }
+    });
+    };
+
+    $star_rating.on('click', function() {
+    $star_rating.siblings('input.rating-value').val($(this).data('rating'));
+    return SetRatingStar();
+    });
+
+    SetRatingStar();
+    $(document).ready(function() {
+
+    });
 
     // $("header ul.navbar-nav li").click(function (ee){
     //     // ee.preventDefault();
